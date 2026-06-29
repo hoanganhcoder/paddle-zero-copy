bb171eee10dd038e424f1a13103e94587d37ad44
hoanganhcoder <hoanganhfreelancer@gmail.com>
2026-06-30 01:12:55 +0700

    up

--- include/ctc_decode.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <string>
-#include <vector>
-#include "common.hpp"
-
-std::vector<std::string> load_dict(const std::string& path);
-OCRText ctc_decode_cpu(const float* logits, int time_steps, int classes, const std::vector<std::string>& dict);
